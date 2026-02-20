e7281d2520f3d8ddbb7059e709fc966ed4b5e8c4
wayua-hating <nikkiwayua@gmail.com>
2026-02-20 23:03:37 +0300

    made

---
+++ chrome.js
@@ -0,0 +1,9 @@
+function myfunction(){
+    document.getElementById("mybtn").style.backgroundColor="red"
+}
+function test(){
+alert("hello goodmorning")
+}
+function abcd(){
+   document.getElementById("mypara").style.color="purple"
+}
